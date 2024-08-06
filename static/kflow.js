6b04dc460aca19790f01b78de342c3f2dcca9d20
kiran daware <kiran.daware@outlook.com>
2024-08-06 17:04:06 +0530

    modified:   create.html 	modified:   index.html 	modified:   static/kflow.js 	modified:   styles.css 	new file:   xml/uac_hold_B.xml 	new file:   xml/uac_register_quick.xml 	new file:   xml/uas_hold_B.xml 	new file:   xml/uas_register_quick.xml

--- static/kflow.js
+++ static/kflow.js
@@ -3,14 +3,19 @@ const uasXmlList = document.getElementById('uas-list');
 
 function createXmlListItem(xmlName){
     const listItem = document.createElement('li');
+    const spanElm = document.createElement('span');
+    spanElm.textContent = `${xmlName} `;
+    spanElm.setAttribute('data-filename', xmlName);
+    spanElm.classList.add('show-flow');
     // Download link
     const downloadLink = document.createElement('a');
     downloadLink.href = `xml/${xmlName}`;
     downloadLink.download = xmlName; 
-    downloadLink.textContent = `${xmlName}`;
+    downloadLink.className = 'download-button';
+    downloadLink.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="18" width="18" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ff943f" d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>`;
 
     listItem.setAttribute('data-filename', xmlName);
-    listItem.classList.add('xml-li');
+    listItem.classList.add('show-flow');
 
     // // View Diagram link
     // const viewLink = document.createElement('a');
@@ -24,6 +29,7 @@ function createXmlListItem(xmlName){
         listItem.classList.add('selected');
     });
     // Append links to the list item
+    listItem.appendChild(spanElm);
     listItem.appendChild(downloadLink);
     // listItem.appendChild(viewLink);
     return listItem;
@@ -41,17 +47,21 @@ uasXmlFiles.forEach(file => {
 
 // Event listener for View Diagram links
 document.addEventListener('click', async (event) => {
-    if (event.target && event.target.classList.contains('xml-li')) {
+    if (event.target && event.target.classList.contains('show-flow')) {
         event.preventDefault();
+        // Clear the old diagram
+        const container = document.getElementById('diagram');
+        container.innerHTML = '';
+
         const fileName = event.target.getAttribute('data-filename');
         try {
             const xmlText = await fetchXMLFile(fileName);
             const xmlDoc = parseXML(xmlText);
             const umlText = generateUMLText(xmlDoc, fileName);
-            console.log(umlText)
             renderDiagram(umlText);
         } catch (error) {
             console.error("Error:", error);
+            container.innerHTML = `<br>${error}`
         }
     }
 });
@@ -133,9 +143,6 @@ participant farEnd
 }
 
 function renderDiagram(umlText) {
-    const container = document.getElementById('diagram');
-    // Clear the old diagram
-    container.innerHTML = '';
     const diagram = Diagram.parse(umlText);
     diagram.drawSVG('diagram', {theme: 'simple'});
 }
